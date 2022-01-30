6e8e1ea2b62cec0006ed1baf83ba880ccc64fd92
viki <vici.atanasova@gmail.com>
2022-01-30 22:16:43 +0200

    Added new console interactions

--- src/main/java/automation/test.java
+++ src/main/java/automation/test.java
@@ -1,4 +1,33 @@
 package automation;
 
+import java.util.Scanner;
+
 public class test {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter your first name");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+
+        //System.out.println("Your name is: " + name);
+
+        System.out.println(" Please enter your age: ");
+
+        int userAge = scanner.nextInt();
+
+        System.out.println(" Your name is: " + name +  "\nYour age is: " + userAge);
+
+        if (userAge >= 18 ) {
+
+            System.out.println( name + " User can drink alcohol legally.");
+        }
+
+        if (userAge < 18 ) {
+
+            System.out.println( name + " User can drink milk.");
+        }
+
+    }
 }
